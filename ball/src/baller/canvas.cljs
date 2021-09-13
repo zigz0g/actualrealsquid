ab7a3f251bf40af70a210f28bbe8b493a945e858
zigzzag <59981732+zigz0g@users.noreply.github.com>
2021-09-13 06:59:21 -0400

    Delete ball directory

--- ball/src/baller/canvas.cljs
+++
@@ -1,9 +0,0 @@
-(ns baller.canvas
-   (:require [infinitelives.pixi.canvas :as canvas]
-             [baller.constants :refer [canvas-colour]]))
-
-(defonce canvas
-  (canvas/init {:layers [:bg :ball :ui :score]
-           :background canvas-colour
-           :expand true
-           :origins {:score :top-right}}))
